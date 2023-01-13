5f97f5eabcc1f9425c99caeaa1af056b66146fb6
Sergey Cherkashin <cherkashin@vuflya.com>
2023-01-13 14:51:10 +0300

    add pong

--- app/main.js
+++ app/main.js
@@ -5,7 +5,21 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+  console.log('client connected');
+
+  connection.on('data', (data) => {
+    connection.write('+PONG\r\n');
+    connection.end();
+  });
+
+  connection.on('end', () => {
+    console.log('client disconnected');
+  });
+
+});
+
+server.on('error', (err) => {
+  throw err;
 });
 
 server.listen(6379, "127.0.0.1");
